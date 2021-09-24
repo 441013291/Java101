2fe84b9b7e84a137ceda10fbe08c000bd9034e4c
Abdullh Alhatmi <91118396+AbdullhAlhatmi@users.noreply.github.com>
2021-09-24 15:35:03 +0300

    Update Assignment1.java

--- Assignment1.java
+++ Assignment1.java
@@ -11,3 +11,159 @@ class Main {
       System.out.println(var1+var2);
   }
 }
+
+
+import java.util.Scanner;
+//Your Name Abdullh Alhatmi
+//Your ID 441013291
+class Main {
+  public static void main(String[] args) {
+      //Start your code 
+      float var1,var2;
+      Scanner myScanner = new Scanner(System.in);
+      var1 = myScanner.nextFloat();
+      var2 = myScanner.nextFloat();
+      System.out.println(var1-var2);
+  }
+}
+
+import java.util.Scanner;
+//Your Name Abdullh Alhatmi
+//Your ID 441013291
+class Main {
+  public static void main(String[] args) {
+      //Start your code 
+      float var1,var2;
+      Scanner myScanner = new Scanner(System.in);
+      var1 = myScanner.nextFloat();
+      var2 = myScanner.nextFloat();
+      System.out.println(var1*var2);
+  }
+}
+
+import java.util.Scanner;
+//Your Name Abdullh Alhatmi
+//Your ID 441013291
+class Main {
+  public static void main(String[] args) {
+      //Start your code 
+      float var1,var2;
+      Scanner myScanner = new Scanner(System.in);
+      var1 = myScanner.nextFloat();
+      var2 = myScanner.nextFloat();
+      System.out.println(var1/var2);
+  }
+}
+
+import java.util.Scanner;
+//Your Name Abdullh Alhatmi
+//Your ID 441013291
+class Main {
+  public static void main(String[] args) {
+      //Start your code 
+      float var1,var2;
+      Scanner myScanner = new Scanner(System.in);
+      var1 = myScanner.nextFloat();
+      var2 = myScanner.nextFloat();
+      System.out.println(var1>var2);
+  }
+}
+
+import java.util.Scanner;
+//Your Name Abdullh Alhatmi
+//Your ID 441013291
+class Main {
+  public static void main(String[] args) {
+      //Start your code 
+      float var1,var2;
+      Scanner myScanner = new Scanner(System.in);
+      var1 = myScanner.nextFloat();
+      var2 = myScanner.nextFloat();
+      System.out.println(var1<var2);
+  }
+}
+
+import java.util.Scanner;
+//Your Name Abdullh Alhatmi
+//Your ID 441013291
+class Main {
+  public static void main(String[] args) {
+      //Start your code 
+      float var1,var2;
+      Scanner myScanner = new Scanner(System.in);
+      var1 = myScanner.nextFloat();
+      var2 = myScanner.nextFloat();
+      System.out.println(var1%var2);
+  }
+}
+
+import java.util.Scanner;
+//Your Name Abdullh Alhatmi
+//Your ID 441013291
+class Main {
+  public static void main(String[] args) {
+      //Start your code 
+      float var1,var2;
+      Scanner myScanner = new Scanner(System.in);
+      var1 = myScanner.nextFloat();
+      var2 = myScanner.nextFloat();
+      System.out.println(var1>=var2);
+  }
+}
+
+import java.util.Scanner;
+//Your Name Abdullh Alhatmi
+//Your ID 441013291
+class Main {
+  public static void main(String[] args) {
+      //Start your code 
+      float var1,var2;
+      Scanner myScanner = new Scanner(System.in);
+      var1 = myScanner.nextFloat();
+      var2 = myScanner.nextFloat();
+      System.out.println(var1<=var2);
+  }
+}
+
+import java.util.Scanner;
+//Your Name Abdullh Alhatmi
+//Your ID 441013291
+class Main {
+  public static void main(String[] args) {
+      //Start your code 
+      float var1,var2;
+      Scanner myScanner = new Scanner(System.in);
+      var1 = myScanner.nextFloat();
+      var2 = myScanner.nextFloat();
+      System.out.println(var1!=var2);
+  }
+}
+
+import java.util.Scanner;
+//Your Name Abdullh Alhatmi
+//Your ID 441013291
+class Main {
+  public static void main(String[] args) {
+      //Start your code 
+      float var1,var2;
+      Scanner myScanner = new Scanner(System.in);
+      var1 = myScanner.nextFloat();
+      var2 = myScanner.nextFloat();
+      System.out.println(var1==var2);
+  }
+}
+
+import java.util.Scanner;
+//Your Name Abdullh Alhatmi
+//Your ID 441013291
+class Main {
+  public static void main(String[] args) {
+      //Start your code 
+      float var1,var2;
+      Scanner myScanner = new Scanner(System.in);
+      var1 = myScanner.nextFloat();
+      var2 = myScanner.nextFloat();
+      System.out.println(var1*/var2);
+  }
+}
+
