5c35638c515cd365cb7ccf2824b80ded8974a783
Abdullh Alhatmi <91118396+AbdullhAlhatmi@users.noreply.github.com>
2021-09-24 03:01:20 +0300

    Create Assignment1.java

---
+++ Assignment1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+//Your Name Abdullh Alhatmi
+//Your ID 441013291
+class Main {
+  public static void main(String[] args) {
+      //Start your code 
+      float var1,var2;
+      Scanner myScanner = new Scanner(System.in);
+      var1 = myScanner.nextFloat();
+      var2 = myScanner.nextFloat();
+      System.out.println(var1+var2);
+  }
+}
