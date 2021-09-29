8dc1beca24be8f25ee6bad98db44da454e35a04d
Hani M. Th <hmthubaiti@uqu.edu.sa>
2021-09-29 11:41:03 +0300

    استخدم كلاس واحد داخل كل ملف جافا داخل كل كلاس استخدم دالة وحده للتنفي١ وهي الرئيسية

--- Assignment1.java
+++ Assignment1.java
@@ -1,4 +1,7 @@
 import java.util.Scanner;
+//your grade is 1/5
+//@ use only one class in a single file.
+//@ use onlye one main in one class.
 //Your Name Abdullh Alhatmi
 //Your ID 441013291
 class Main {
