5d0996d954e7f2f6b05d6a875d6e9c5b256ff566
Abdullh Alhatmi <91118396+441013291@users.noreply.github.com>
2021-10-20 21:00:50 +0300

    Create mid.java

---
+++ mid.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Mid {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("name");
+		String name =scan.nextLine();
+		
+ 		System.out.println("age");
+ 		int age =scan.nextInt();
+ 		
+ 		System.out.println("year of birth");
+ 	    int year = scan.nextInt();
+ 	  	System.out.println("var");
+ 	    int var = scan.nextInt();
+ 	    	System.out.println(year-var);
+  }
+}
