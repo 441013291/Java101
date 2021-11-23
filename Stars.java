faee3bf4acf8dce5406a9da640fb1c760eb18a25
Abdullh Alhatmi <91118396+441013291@users.noreply.github.com>
2021-11-24 00:36:35 +0300

    Create Stars.java

---
+++ Stars.java
@@ -0,0 +1,15 @@
+public class Main {
+	public static void main(String[] args) {
+	 int counterX = 5;
+	 while(counterX >=1){
+	     int counterY = 1;
+	     while (counterY <=counterX){
+	         System.out.print("*");
+	         ++counterY;
+	     }
+	     System.out.println();
+	     --counterX;
+	    
+	 }
+	}
+}
