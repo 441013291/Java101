bfd8e8a27331e0bfd7027e1d002aea8ce81a8bd1
Abdullh Alhatmi <91118396+441013291@users.noreply.github.com>
2021-11-24 01:50:01 +0300

    Create age.java

---
+++ age.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class age {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int age1 = scan.nextInt();
+        System.out.println("Your age is ="+ (2021 - age1));
+    }
+}
