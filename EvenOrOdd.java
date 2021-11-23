fb6a31ddd218852b4ba35210ce975360f0a45fba
Abdullh Alhatmi <91118396+441013291@users.noreply.github.com>
2021-11-24 00:57:00 +0300

    Create EvenOrOdd.java

---
+++ EvenOrOdd.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class EvenOrOdd {
+	public static void main(String[] args) {
+	     Scanner scan = new Scanner(System.in);
+	     int var = scan.nextInt();
+	     if(var%2==0)
+	     System.out.println("Even");
+	     else 
+	     System.out.println("Odd");
+	}
+}
+
