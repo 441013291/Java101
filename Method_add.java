6ceec8acac39bd35313f5e4a3b135f2fb609bb02
Abdullh Alhatmi <91118396+441013291@users.noreply.github.com>
2021-11-24 04:26:47 +0300

    Create Method_add.java

---
+++ Method_add.java
@@ -0,0 +1,23 @@
+public class Method_add {
+  static int add(int x, int y) {
+    return x + y;
+  }
+
+ static int Sub(int x, int y) {
+    return x - y;
+  }
+   static int multi(int x, int y) {
+    return x * y;
+   }
+    static int dif(int x, int y) {
+    return x / y;
+    }
+  public static void main(String[] args) {
+    
+    System.out.println(add(1,5));
+        System.out.println(Sub(1,5));
+         System.out.println(multi(1,5));
+      System.out.println(dif(1,5));
+  }
+  
+  
