957c0ca83cb0d163c89a856282f44968ff17040c
Abdullh Alhatmi <91118396+441013291@users.noreply.github.com>
2021-11-24 04:19:22 +0300

    Create Method.java

---
+++ Method.java
@@ -0,0 +1,23 @@
+public class Method {
+    static void a(String name , int age ){
+        System.out.println(name + " is " + age );
+    }
+    public static void main(String [] args){ 
+        a("ever" , 30 );
+        a("banega" , 31);
+         a("ever" , 32 );
+          a("ever" , 33 );
+           a("ever" , 34 );
+            a("ever" , 35 );
+             a("ever" , 36 );
+              a("ever" , 37 );
+               a("ever" , 38 );
+                a("ever" , 39 );
+                 a("ever" , 40 );
+                  a("ever" , 41 );
+                   a("ever" , 42);
+                    a("ever" , 43 );
+                     a("ever" , 44);
+                      a("ever" , 45 );
+    }
+}
