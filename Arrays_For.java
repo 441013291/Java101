df6d6cb2aa6007a1af14d1890f0bf067bea66406
Abdullh Alhatmi <91118396+441013291@users.noreply.github.com>
2021-11-25 18:34:41 +0300

    Create Arrays_For.java

---
+++ Arrays_For.java
@@ -0,0 +1,7 @@
+public class Arrays_For {
+	public static void main(String[] args) {
+	String [] name = {"hani" , "abdullh" , "khaled" , "hussen" };
+	for(int a = 0; a<name.length; a++)
+	      System.out.println(name[a]);
+	}
+}
