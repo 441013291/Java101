fa245247cea24570ad947fd32bf902d8e7a4565d
Abdullh Alhatmi <91118396+441013291@users.noreply.github.com>
2021-11-25 16:55:34 +0300

    Create Offer.java

---
+++ Offer.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Offer {
+	public static void main(String[] args) {
+	    Scanner scan = new Scanner (System.in);
+		int offer = scan.nextInt();
+		if(offer >= 5000000)
+		System.out.println("i agree");
+		else
+		System.out.println("i refuse");
+	}
+}
+
+// Offer the player a minimum annual salary of 5000000
