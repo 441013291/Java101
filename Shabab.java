b2efd87b43fa458b1fd04add25ecc6b270fc9245
Abdullh Alhatmi <91118396+441013291@users.noreply.github.com>
2021-11-25 16:47:26 +0300

    Create Shabab.java

---
+++ Shabab.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Shabab {
+	public static void main(String[] args) {
+	   Scanner a = new Scanner(System.in);
+	   int ALSHABAB = a.nextInt();
+	    if(ALSHABAB == 1){
+	        System.out.println("FWAZ AL QARNI");
+	    }else if(ALSHABAB == 2){ 
+	        System.out.println("IGOUR");
+	    }else if(ALSHABAB == 8){ 
+	        System.out.println("PHAWLINHO");
+	    }else if(ALSHABAB == 10){
+	        System.out.println("EVER BANEGA");
+	    }else if(ALSHABAB == 13){ 
+	        System.out.println("CARLOS GR");
+	    }else{ 
+	        System.out.println("CAMOSCA");
+	    
+	    }
+	}
+}
