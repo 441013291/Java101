26332b9e74f401a253f9dd318aa883208d85bcee
Abdullh Alhatmi <91118396+441013291@users.noreply.github.com>
2021-11-25 16:04:03 +0300

    Create Ood.java

---
+++ Ood.java
@@ -0,0 +1,12 @@
+public class Ood {
+	public static void main(String[] args) {
+		 int index = 0;
+		 while(index < 100){
+		     ++index;
+		     if(index%2==0)
+		     continue;
+		     System.out.println(index);
+		 }
+	}
+}
+
