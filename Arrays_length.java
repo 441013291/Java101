bae743301ca0e3fd444815b3a2906febc18eb547
Abdullh Alhatmi <91118396+441013291@users.noreply.github.com>
2021-11-25 18:30:30 +0300

    Create Arrays_length.java

---
+++ Arrays_length.java
@@ -0,0 +1,7 @@
+public class Arrays_length {
+	public static void main(String[] args) {
+	String [] name = {"hani" , "abdullh" , "khaled" , "hussen" };
+	      System.out.println(name.length);
+	
+	}
+}
