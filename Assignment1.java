a9e1dda3b2456e27dd706aa2d0db54c2a5bff9ce
Abdullh Alhatmi <91118396+441013291@users.noreply.github.com>
2021-12-13 23:11:46 +0300

    Update Assignment1.java

--- Assignment1.java
+++ Assignment1.java
@@ -1,10 +1,8 @@
 import java.util.Scanner;
-//your grade is 1/5
-//@ use only one class in a single file.
-//@ use onlye one main in one class.
 //Your Name Abdullh Alhatmi
 //Your ID 441013291
-class Main {
+import java.util.Scanner;
+class Assignment1 {
   public static void main(String[] args) {
       //Start your code 
       float var1,var2;
@@ -12,148 +10,14 @@ class Main {
       var1 = myScanner.nextFloat();
       var2 = myScanner.nextFloat();
       System.out.println(var1+var2);
-  }
-}
-
-
-import java.util.Scanner;
-//Your Name Abdullh Alhatmi
-//Your ID 441013291
-class Main {
-  public static void main(String[] args) {
-      //Start your code 
-      float var1,var2;
-      Scanner myScanner = new Scanner(System.in);
-      var1 = myScanner.nextFloat();
-      var2 = myScanner.nextFloat();
       System.out.println(var1-var2);
-  }
-}
-
-import java.util.Scanner;
-//Your Name Abdullh Alhatmi
-//Your ID 441013291
-class Main {
-  public static void main(String[] args) {
-      //Start your code 
-      float var1,var2;
-      Scanner myScanner = new Scanner(System.in);
-      var1 = myScanner.nextFloat();
-      var2 = myScanner.nextFloat();
       System.out.println(var1*var2);
-  }
-}
-
-import java.util.Scanner;
-//Your Name Abdullh Alhatmi
-//Your ID 441013291
-class Main {
-  public static void main(String[] args) {
-      //Start your code 
-      float var1,var2;
-      Scanner myScanner = new Scanner(System.in);
-      var1 = myScanner.nextFloat();
-      var2 = myScanner.nextFloat();
-      System.out.println(var1/var2);
-  }
-}
-
-import java.util.Scanner;
-//Your Name Abdullh Alhatmi
-//Your ID 441013291
-class Main {
-  public static void main(String[] args) {
-      //Start your code 
-      float var1,var2;
-      Scanner myScanner = new Scanner(System.in);
-      var1 = myScanner.nextFloat();
-      var2 = myScanner.nextFloat();
-      System.out.println(var1>var2);
-  }
-}
-
-import java.util.Scanner;
-//Your Name Abdullh Alhatmi
-//Your ID 441013291
-class Main {
-  public static void main(String[] args) {
-      //Start your code 
-      float var1,var2;
-      Scanner myScanner = new Scanner(System.in);
-      var1 = myScanner.nextFloat();
-      var2 = myScanner.nextFloat();
-      System.out.println(var1<var2);
-  }
-}
-
-import java.util.Scanner;
-//Your Name Abdullh Alhatmi
-//Your ID 441013291
-class Main {
-  public static void main(String[] args) {
-      //Start your code 
-      float var1,var2;
-      Scanner myScanner = new Scanner(System.in);
-      var1 = myScanner.nextFloat();
-      var2 = myScanner.nextFloat();
       System.out.println(var1%var2);
-  }
-}
-
-import java.util.Scanner;
-//Your Name Abdullh Alhatmi
-//Your ID 441013291
-class Main {
-  public static void main(String[] args) {
-      //Start your code 
-      float var1,var2;
-      Scanner myScanner = new Scanner(System.in);
-      var1 = myScanner.nextFloat();
-      var2 = myScanner.nextFloat();
-      System.out.println(var1>=var2);
-  }
-}
-
-import java.util.Scanner;
-//Your Name Abdullh Alhatmi
-//Your ID 441013291
-class Main {
-  public static void main(String[] args) {
-      //Start your code 
-      float var1,var2;
-      Scanner myScanner = new Scanner(System.in);
-      var1 = myScanner.nextFloat();
-      var2 = myScanner.nextFloat();
-      System.out.println(var1<=var2);
-  }
-}
-
-import java.util.Scanner;
-//Your Name Abdullh Alhatmi
-//Your ID 441013291
-class Main {
-  public static void main(String[] args) {
-      //Start your code 
-      float var1,var2;
-      Scanner myScanner = new Scanner(System.in);
-      var1 = myScanner.nextFloat();
-      var2 = myScanner.nextFloat();
+      System.out.println(var1/var2);
       System.out.println(var1!=var2);
+      System.out.println(var1<=var2);
+      System.out.println(var1>=var2);
+       System.out.println(var1<var2);
+        System.out.println(var1>var2);
   }
 }
-
-import java.util.Scanner;
-//Your Name Abdullh Alhatmi
-//Your ID 441013291
-class Main {
-  public static void main(String[] args) {
-      //Start your code 
-      float var1,var2;
-      Scanner myScanner = new Scanner(System.in);
-      var1 = myScanner.nextFloat();
-      var2 = myScanner.nextFloat();
-      System.out.println(var1==var2);
-  }
-}
-
-
