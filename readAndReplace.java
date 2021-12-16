0c86329ffd67eb896823a63249e4d87e9e5ba5c5
Abdullh Alhatmi <91118396+441013291@users.noreply.github.com>
2021-12-16 22:08:24 +0300

    Create readAndReplace.java

---
+++ readAndReplace.java
@@ -0,0 +1,16 @@
+public class readAndReplace{
+public static void main(String[] args) {
+     String a = "------------------";
+     for (int x =0;  x<=8 ; ++x){
+         if(a.charAt(x) == '-'){
+        System.out.println('x');}
+        else {
+            System.out.println(a.charAt(x));
+        }
+        
+     }
+    
+}
+    
+}
+    
