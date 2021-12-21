186e377e6a7b923325f98763e20382cd3cd92b70
Abdullh Alhatmi <91118396+441013291@users.noreply.github.com>
2021-12-21 08:40:33 +0300

    Create Exam.java

---
+++ Exam.java
@@ -0,0 +1,17 @@
+public class Main {
+	public static void main(String[] args) {
+	   
+	    for(int counter = 1 ; counter<=100; counter++ ){
+	    
+	        if(counter%2!=0) 
+	          System.out.println(counter + " % is odd");
+	          else
+	            System.out.println(counter +" % is even");
+
+	    
+	
+}
+	    
+
+	}
+}
